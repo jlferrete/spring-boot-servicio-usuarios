ec62bc10e7c373c4c92455e5b594719fe6181114
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-07-02 12:13:58 +0200

    Created the many to many relationship between Usuario and Role

--- src/main/java/com/formacionbdi/springboot/app/usuarios/models/entity/Usuario.java
+++ src/main/java/com/formacionbdi/springboot/app/usuarios/models/entity/Usuario.java
@@ -1,13 +1,19 @@
 package com.formacionbdi.springboot.app.usuarios.models.entity;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name="usuarios")
@@ -29,6 +35,21 @@ public class Usuario implements Serializable {
 
 	@Column(unique = true, length = 100)
 	private String email;
+	
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name="usuarios_to_roles", joinColumns = @JoinColumn(name="usuario_id"), 
+	inverseJoinColumns = @JoinColumn(name="role_id"),
+	uniqueConstraints = {@UniqueConstraint(columnNames = {"usuario_id" , "role_id"})})
+	
+	private List<Role> roles;
+	
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
 
 	public Long getId() {
 		return id;
@@ -86,6 +107,10 @@ public class Usuario implements Serializable {
 		this.email = email;
 	}
 	
+	
+
+
+
 	private static final long serialVersionUID = 4002221912401133094L;
 
 }
