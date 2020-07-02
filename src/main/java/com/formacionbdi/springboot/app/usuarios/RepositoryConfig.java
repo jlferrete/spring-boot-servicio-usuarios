474052aaa8bff8d1dfab8363db26a4fe0b778e18
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-07-02 19:57:39 +0200

    Setting up and exposing id in the json response

---
+++ src/main/java/com/formacionbdi/springboot/app/usuarios/RepositoryConfig.java
@@ -0,0 +1,20 @@
+package com.formacionbdi.springboot.app.usuarios;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import com.formacionbdi.springboot.app.usuarios.models.entity.Role;
+import com.formacionbdi.springboot.app.usuarios.models.entity.Usuario;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Usuario.class, Role.class);
+	}
+
+	
+	
+}
